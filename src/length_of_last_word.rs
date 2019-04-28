adf6a6fc4178a23176e2841f80fdb51c2374fa93
wulu <wulu90@outlook.com>
2019-04-28 21:58:57 +0800

    another solution from string's end to start, use rust std lib

--- src/length_of_last_word.rs
+++ src/length_of_last_word.rs
@@ -20,6 +20,29 @@ impl Solution {
             prelen
         }
     }
+
+    pub fn length_of_last_word_fromback(s: String) -> i32 {
+        let mut len = 0;
+        let mut isfind = false;
+        for ch in s.chars().rev() {
+            if ch == ' ' && isfind {
+                break;
+            }
+            if ch != ' ' {
+                len += 1;
+                isfind = true;
+            }
+        }
+        len
+    }
+
+    pub fn length_of_last_word_stdlib(s: String) -> i32 {
+        s.trim()
+            .split_whitespace()
+            .last()
+            .map(|x| x.len())
+            .unwrap_or(0) as i32
+    }
 }
 
 #[cfg(test)]
@@ -33,18 +56,62 @@ mod test {
             Solution::length_of_last_word(String::from("aslkdjf sajkdfo"))
         );
         assert_eq!(7, Solution::length_of_last_word(String::from("aslkdjf")));
-
         assert_eq!(7, Solution::length_of_last_word(String::from(" sajkdfo")));
-
-        assert_eq!(
-            5,
-            Solution::length_of_last_word(String::from("Hello World"))
-        );
-
         assert_eq!(0, Solution::length_of_last_word(String::from(" ")));
-
         assert_eq!(1, Solution::length_of_last_word(String::from("a")));
         assert_eq!(1, Solution::length_of_last_word(String::from("a       ")));
         assert_eq!(1, Solution::length_of_last_word(String::from("    a   ")));
+    }
+
+    #[test]
+    fn length_of_last_word_fromback() {
+        assert_eq!(
+            7,
+            Solution::length_of_last_word_fromback(String::from("aslkdjf sajkdfo"))
+        );
+        assert_eq!(
+            7,
+            Solution::length_of_last_word_fromback(String::from("aslkdjf"))
+        );
+        assert_eq!(
+            7,
+            Solution::length_of_last_word_fromback(String::from(" sajkdfo"))
+        );
+        assert_eq!(0, Solution::length_of_last_word_fromback(String::from(" ")));
+        assert_eq!(1, Solution::length_of_last_word_fromback(String::from("a")));
+        assert_eq!(
+            1,
+            Solution::length_of_last_word_fromback(String::from("a       "))
+        );
+        assert_eq!(
+            1,
+            Solution::length_of_last_word_fromback(String::from("    a   "))
+        );
+    }
+
+    #[test]
+    fn length_of_last_word_stdlib() {
+        assert_eq!(
+            7,
+            Solution::length_of_last_word_stdlib(String::from("aslkdjf sajkdfo"))
+        );
+        assert_eq!(
+            7,
+            Solution::length_of_last_word_stdlib(String::from("aslkdjf"))
+        );
+        assert_eq!(
+            7,
+            Solution::length_of_last_word_stdlib(String::from(" sajkdfo"))
+        );
+        assert_eq!(0, Solution::length_of_last_word_stdlib(String::from(" ")));
+        assert_eq!(1, Solution::length_of_last_word_stdlib(String::from("a")));
+        assert_eq!(
+            1,
+            Solution::length_of_last_word_stdlib(String::from("a       "))
+        );
+        assert_eq!(
+            1,
+            Solution::length_of_last_word_stdlib(String::from("    a   "))
+        );
     }
 }
