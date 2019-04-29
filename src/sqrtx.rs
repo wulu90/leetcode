5122311a4cbc55317d206ba9e665c78d78b57d73
wulu <wulu90@outlook.com>
2019-04-29 17:48:27 +0800

    Newton–Raphson method

--- src/sqrtx.rs
+++ src/sqrtx.rs
@@ -16,6 +16,16 @@ impl Solution {
         }
         r
     }
+
+    pub fn my_sqrt_newton(x: i32) -> i32 {
+        let mut s: f64 = 1.0;
+        let precision = 0.000001;
+        while (s * s - x as f64).abs() > precision {
+            s = (s + x as f64 / s) / 2.0;
+        }
+        println!("{}", s);
+        s as i32
+    }
 }
 
 #[cfg(test)]
@@ -36,5 +46,21 @@ mod test {
         assert_eq!(100, Solution::my_sqrt(10029));
         assert_eq!(8, Solution::my_sqrt(80));
         assert_eq!(46340, Solution::my_sqrt(2147483647));
+    }
+
+    #[test]
+    fn my_sqrt_newton() {
+        assert_eq!(1, Solution::my_sqrt_newton(1));
+        assert_eq!(1, Solution::my_sqrt_newton(2));
+        assert_eq!(1, Solution::my_sqrt_newton(3));
+        assert_eq!(2, Solution::my_sqrt_newton(4));
+        assert_eq!(2, Solution::my_sqrt_newton(5));
+        assert_eq!(2, Solution::my_sqrt_newton(6));
+        assert_eq!(2, Solution::my_sqrt_newton(7));
+        assert_eq!(2, Solution::my_sqrt_newton(8));
+        assert_eq!(3, Solution::my_sqrt_newton(9));
+        assert_eq!(100, Solution::my_sqrt_newton(10029));
+        assert_eq!(8, Solution::my_sqrt_newton(80));
+        assert_eq!(46340, Solution::my_sqrt_newton(2147483647));
     }
 }
